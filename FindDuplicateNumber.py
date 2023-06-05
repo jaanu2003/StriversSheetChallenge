b05b0a3775b0e358d06f562d0e42e3225ac73368
jahnaviurugonda <93399446+jaanu2003@users.noreply.github.com>
2023-06-05 22:28:15 +0530

    FindDuplicateNumber.py

---
+++ FindDuplicateNumber.py
@@ -0,0 +1,7 @@
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        l = set()
+        for i in nums:
+            if i in l:
+                return i 
+            l.add(i)
